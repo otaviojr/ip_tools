26dced12e22984aa8f5480bdbacec5e7e73719ee
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2022-11-14 00:53:17 -0300

    clean

--- ip_api/imports/client/react/components/BaseApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class BaseApp extends React.Component {
-  render() {
-    return <h1>A base class should not be used!</h1>
-  }
-}
-
-export default BaseApp;
--- ip_api/imports/client/react/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import BaseApp from './BaseApp';
-
-class NotFound extends BaseApp {
-  render() {
-    return <p>:-(</p>;
-  }
-}
-
-export default NotFound;
